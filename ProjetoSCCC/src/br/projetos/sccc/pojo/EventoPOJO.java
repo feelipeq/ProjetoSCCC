84bbc786728c7ac6b55d6f89cfb82b1a8f7a5f35
F0115381 <F0115381@PSGHB777355.portoseguro.brasil>
2017-11-01 18:24:55 -0200

    commit

--- ProjetoSCCC/src/br/projetos/sccc/pojo/EventoPOJO.java
+++ ProjetoSCCC/src/br/projetos/sccc/pojo/EventoPOJO.java
@@ -5,6 +5,10 @@ import java.util.Date;
 
 public class EventoPOJO implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String tecnologia;
 	private String ambiente;
